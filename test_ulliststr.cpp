2e0d9f15a8fefd250e789898d88fb1ccd235406c
Arushi Mundergi <mundergi@usc.edu>
2026-09-10 04:11:07 +0000

    completed part 2

--- test_ulliststr.cpp
+++ test_ulliststr.cpp
@@ -5,9 +5,120 @@
 
 #include "ulliststr.h"
 
+using namespace std;
+
 //Use this file to test your ulliststr implementation before running the test suite
 
 int main(int argc, char* argv[])
 {
+  ULListStr dat; 
 
+  cout << "Back tests" << endl;
+  cout << "Starting size - expected 0: " << dat.size() << endl;
+
+  cout << "Starting empty - expected 1: " << dat.empty() << endl;
+  
+  //popping empty list
+  dat.pop_back(); 
+
+  //push_back on empty list
+  dat.push_back("0"); 
+
+  //push_back when tail has space
+  dat.push_back("1"); 
+  dat.push_back("2");
+  dat.push_back("3");
+  dat.push_back("4");
+  dat.push_back("5");
+  dat.push_back("6");
+  dat.push_back("7");
+  dat.push_back("8");
+  dat.push_back("9");
+
+  //first node is full so create new tail node
+  dat.push_back("10");
+
+  cout << "Expected: 0 1 2 3 4 5 6 7 8 9 10" << endl;
+  cout << "Actual: "; 
+
+  for (size_t i = 0; i < dat.size(); i++){
+    cout << dat.get(i) << " ";
+  }
+  cout << endl; 
+
+  cout << "Front - expected 0: " << dat.front() << endl;
+  cout << "back - expected 10: " << dat.back() << endl; 
+  cout << "Size - expected 11: " << dat.size() << endl; 
+
+  //pop back 
+  dat.pop_back(); 
+  cout << "After pop_back - expected 9: " << dat.back() << endl;
+  cout << "Size - expected 10: " << dat.size() << endl; 
+
+  //tail contains multiple vals 
+  dat.pop_back();
+  cout << "After pop_back - expected 8: " << dat.back() << endl;
+  cout << "Size - expected 9: " << dat.size() << endl;
+  
+  while (!dat.empty()){
+    dat.pop_back();
+  }
+
+  cout << "Ending size - expected 0: " << dat.size() << endl; 
+  cout << "Ending empty - expected 1: " << dat.empty() << endl; 
+
+  dat.pop_back(); //pop back when empty again
+
+  cout << endl;
+  cout << "Front tests" << endl; 
+
+  ULListStr datFront;
+  
+  datFront.push_front("0");
+  datFront.push_front("1");
+  datFront.push_front("2");
+  datFront.push_front("3");
+  datFront.push_front("4");
+  datFront.push_front("5");
+  datFront.push_front("6");
+  datFront.push_front("7");
+  datFront.push_front("8");
+  datFront.push_front("9");
+  datFront.push_front("10");
+
+  cout << "Expected: 10 9 8 7 6 5 4 3 2 1 0" << endl;
+  cout << "Actual:  "; 
+
+  for (size_t i = 0; i < datFront.size(); i++){
+    cout << datFront.get(i) << " "; 
+  }
+  cout << endl; 
+
+  cout << "Front - expected 10: " << datFront.front() << endl; 
+  cout << "Back - expected 0: " << datFront.back() << endl; 
+  cout << "Size - expected 11: " << datFront.size() << endl; 
+
+  datFront.pop_front(); 
+
+  cout << "After pop_front - expected 9: " << datFront.front() << endl;
+  cout << "Size - expected 10: " << datFront.size() << endl; 
+
+  datFront.pop_front(); 
+
+  cout << "After second pop_front - expected 8: " << datFront.front() << endl;
+  cout << "Size - expected 9: " << datFront.size() << endl; 
+
+  datFront.set(0, "changed");
+
+  cout << "After set, get(0) expected changed: " << datFront.get(0) << endl; 
+
+  while (!datFront.empty()) {
+    datFront.pop_front(); 
+  }
+
+  cout << "Ending size - expected 0: " << datFront.size() << endl; 
+  cout << "Ending empty - expected 1: " << datFront.empty() << endl; 
+
+  datFront.pop_front(); 
+  return 0; 
 }
--- ulliststr.cpp
+++ ulliststr.cpp
@@ -2,6 +2,8 @@
 #include <stdexcept>
 #include "ulliststr.h"
 
+using namespace std; 
+
 ULListStr::ULListStr()
 {
   head_ = NULL;
@@ -25,6 +27,146 @@ size_t ULListStr::size() const
 }
 
 // WRITE YOUR CODE HERE
+//push front - check if head node exists and if array has free space shift everything one 
+//one to the right, and then add new value at zero, set to head, connect head->next to old head
+//if no space, create a new node and connect it to the old head as new head
+
+//push back - check if tail node exists and if array has free space add new val at the end 
+//if no space, create new node add val and connect to old tail 
+
+//pop front - check if list is empty - if empty return 
+//else find value of current head node, store it for deleting later, then move everything 
+//one to the left 
+
+//front 
+const string& ULListStr::front() const {
+  return head_->val[head_->first]; 
+}
+
+//back 
+const string& ULListStr::back() const {
+  return tail_->val[tail_->last - 1]; 
+}
+
+void ULListStr::push_front(const string& val){
+  if (size_ == 0){
+    Item* newNode = new Item(); 
+    //create a new node using the back of last node
+    newNode->first = ARRSIZE - 1; //where to store first value of new node
+    newNode->last = ARRSIZE; 
+    newNode->val[newNode->first] = val; 
+
+    //both bc list is empty and new node 
+    head_ = newNode; 
+    tail_ = newNode; 
+  }
+  else if(head_->first > 0){
+    head_->first--;
+    head_->val[head_->first] = val; 
+  }
+  else {
+    //create a new node and attach it
+    Item* newNode = new Item();
+    newNode->first = ARRSIZE - 1;
+    newNode->last = ARRSIZE; 
+    newNode->val[newNode->first] = val; 
+
+    newNode->next = head_; //attaches current head to newNode
+    head_->prev = newNode;
+    head_ = newNode; 
+  }
+  size_++; //because size changes by one by adding an element
+}
+
+void ULListStr::push_back(const string& val){
+  if (size_ == 0){
+    Item* newNode = new Item(); 
+
+    newNode->first = 0;
+    newNode->last = 1; 
+    newNode->val[0] = val; 
+
+    head_ = newNode; 
+    tail_ = newNode;
+  }
+  else if (tail_->last < ARRSIZE){ //arrsize is size of arr so if tail has space its index will be less than size
+    //tail has room after last value
+    tail_->val[tail_->last] = val; 
+    tail_->last++; 
+  }
+
+  else {
+    Item* newNode = new Item(); 
+    newNode->first = 0; 
+    newNode->last = 1;
+    newNode->val[0] = val; 
+
+    //connect old tail to new node 
+    newNode->prev = tail_; 
+    tail_->next = newNode; 
+    tail_ = newNode;  
+  }
+  size_++; 
+}
+
+void ULListStr::pop_front(){
+  if (size_ == 0){
+    return; 
+  } else if (head_->last - head_->first > 1){ //if more than one val
+      head_->first++; //move head's first one to the right
+  } else {
+      Item* temp = head_; 
+      head_ = head_->next; 
+
+      if (head_ == NULL){
+        tail_ = NULL; 
+      } else {
+        head_->prev = NULL; 
+      }
+      delete temp; 
+    }
+  size_--; 
+}
+
+void ULListStr::pop_back(){
+  if (size_ == 0){
+    return;
+  } else if (tail_->last - tail_->first > 1){
+    tail_->last--; //move index back one 
+  } else {
+    Item* temp = tail_; 
+
+    tail_ = tail_->prev; 
+
+    if (tail_ == NULL){
+      head_ = NULL; 
+    } else {
+      tail_->next = NULL;
+    }
+    delete temp; 
+  }
+  size_--; 
+}
+
+string* ULListStr::getValAtLoc(size_t loc) const {
+  if (loc >= size_){
+    return NULL; 
+  }
+
+  Item* curr = head_; 
+
+  while (curr != NULL){
+    size_t nodeSize = curr->last - curr->first; 
+
+    if (loc < nodeSize){
+      return &(curr->val[curr->first + loc]); //return current value's array's element at loc
+    }
+
+    loc = loc - nodeSize;
+    curr = curr->next; //to continue thru loop 
+  }
+  return NULL; 
+}
 
 void ULListStr::set(size_t loc, const std::string& val)
 {
